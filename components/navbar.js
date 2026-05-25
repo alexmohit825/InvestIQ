8fdfdf907947497acbf252046e79fcfc92f6f8d2
alexmohit825 <alex.mohit@icloud.com>
2026-05-24 20:37:31 -0700

    Add renderNavbar function to create navbar

---
+++ components/navbar.js
@@ -0,0 +1,10 @@
+export function renderNavbar() {
+  document.getElementById("navbar").innerHTML = `
+    <div class="logo">InvestIQ 2.0</div>
+    <div class="nav-links">
+      <a href="#">Dashboard</a>
+      <a href="#">Portfolio</a>
+      <a href="#">Settings</a>
+    </div>
+  `;
+}
